5234234f941b88568b0c408d20ead8abb052fbe5
sindhura <sindhura343434@gmail.com>
2023-04-25 13:34:32 +0530

    Method -overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,14 @@ public class Main {
 
   }
 
-  static class A {
+  public static class A {
     public static void meth() {
 
       System.out.println("Invoking method from class A");
     }
   }
 
-  static class B extends A {
+ public  static class B extends A {
     public static void meth() {
       System.out.println("Method is overridden in Extendend class B");
     }
