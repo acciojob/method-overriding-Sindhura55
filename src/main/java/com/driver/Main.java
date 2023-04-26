f98857960f90f2fc3420308ca68bfc4b90c02873
sindhura <sindhura343434@gmail.com>
2023-04-26 15:26:36 +0530

    Changed return type of meth method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,21 +3,24 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-    B obj= new  B();
+    B obj= new B();
     obj.meth();
+    B obj1=new B();
+    obj1.meth();
 
   }
 
-  public static class A {
-    public static void meth() {
-
-      System.out.println("Invoking method from class A");
+   public static class A {
+    public static String meth()
+    {
+      return "Invoking method from class A";
     }
   }
 
- public  static class B extends A {
-    public static void meth() {
-      System.out.println("Method is overridden in Extendend class B");
+ public static class B extends A {
+    public static String meth()
+    {
+        return "Method is overridden in Extendend class B";
     }
   }
 }
